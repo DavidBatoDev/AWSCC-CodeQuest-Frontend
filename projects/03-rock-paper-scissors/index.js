d33f52faae3dfb62d56f783899b52cc2467db129
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-03-28 04:12:02 +0800

    Finished Project 3: Rock Paper Scissor game

--- projects/03-rock-paper-scissors/index.js
+++ projects/03-rock-paper-scissors/index.js
@@ -1,36 +1,129 @@
-// Step 1: Define the choices available in the game
-const choices = ["rock", "paper", "scissors"];
+const PREFIX_KEY = 'rock-paper-scissor';
+const scores = JSON.parse(localStorage.getItem(PREFIX_KEY + 'scores'));
+const circlePlayer = document.querySelector('.circle-player');
+const circleComputer = document.querySelector('.circle-computer');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const resultHTML = document.querySelector('.result');
+const playButton = document.querySelector('.play-btn');
+const restartButton = document.querySelector('.restart-btn');
+const autoPlayButton = document.querySelector('.auto-play');
+const stopAutoPlayButton = document.querySelector('.stop-auto');
+let playerScoreElement = document.querySelector('.player-score');
+let computerScoreElement = document.querySelector('.computer-score');
+let intervalId;
+let playerMove = '';
+let playerScore = scores ? scores.user : 0;
+let computerScore = scores ? scores.computer : 0;
+playerScoreElement.textContent = playerScore;
+computerScoreElement.textContent = computerScore;
 
-// Step 2: Get references to HTML elements
-const player1ChoiceDisplay = // Get the element with the id of "player1-choice-display"
-const player1ScoreDisplay = // Get the element with the id of "player1-score"
-const player2ChoiceDisplay = // Get the element with the id of "player2-choice-display"
-const player2ScoreDisplay = // Get the element with the id of "player2-score"
-
-const gameStatusDisplay = // Get the element with the id of "game-status"
-const resetGameButton = // Get the element with the id of "reset-game-btn"
-
-const rockBtn = // Get the element with the id of "rock-btn"
-const paperBtn = // Get the element with the id of "paper-btn"
-const scissorsBtn = // Get the element with the id of "scissors-btn"
-
-// Step 3: Add event listeners to the buttons
-rockBtn.addEventListener("click", function () {
-
+rock.addEventListener('click', () => pickMove('rock'));
+paper.addEventListener('click', () => pickMove('paper'));
+scissor.addEventListener('click', () => pickMove('scissor'));
+restartButton.addEventListener('click', () => {
+    localStorage.removeItem('scores');
+    playerScore = 0;
+    computerScore = 0;
+    circlePlayer.innerHTML = '?'
+    circleComputer.innerHTML = '?'
+    resultHTML.innerHTML = '';
+    updateScores();
+});
+circlePlayer.addEventListener('click', () => {
+    circlePlayer.innerHTML = '?'
+    circleComputer.innerHTML = '?'
+    resultHTML.innerHTML = '';
+    playerMove = '';
+});
+playButton.addEventListener('click', handlePlayRound);
+playButton.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        handlePlayRound();
+    }
+});
+autoPlayButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    stopAutoPlayButton.classList.remove('hidden');
+    autoPlayButton.classList.add('hidden');
+    intervalId = setInterval(autoPlay, 1000);
+});
+stopAutoPlayButton.addEventListener('click', () => {
+    clearInterval(intervalId)
+    stopAutoPlayButton.classList.add('hidden');
+    autoPlayButton.classList.remove('hidden');
 });
 
-paperBtn.addEventListener("click", function () {
 
-});
+function pickMove(move) {
+    circlePlayer.classList.remove('animate');
+    circlePlayer.classList.add('animate');
+    circlePlayer.innerHTML = `<img class="move" src="./assets/${move}.png" alt="${move}">`;
+    playerMove = move;
+    resultHTML.innerHTML = '';
+    circleComputer.innerHTML = '?';
+}
 
-scissorsBtn.addEventListener("click", function () {
+function computerPlay() {
+    let moves = ['rock', 'paper', 'scissor'];
+    let random = Math.floor(Math.random() * moves.length);
+    return moves[random];
+}
 
-});
+function playRound(playerSelection, computerSelection) {
+    if (!playerSelection) return alert('Player has no move!');
+    let result = '';
+    let htmlForComputer = '';
+    let htmlForResult = '';
 
-resetGameButton.addEventListener("click", function () { 
-    
-});
+    if (playerSelection === computerSelection) {
+        result = 'It\'s a Draw!';
+        playerScore++;
+        computerScore++;
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')
+    ) {
+        result = 'Player Wins!';
+        playerScore++;
+    } else {
+        result = 'Computer Wins!';
+        computerScore++;
+    }
 
-// Step 4: Define helper functions that will be the actions of the game
+    htmlForResult = `
+        <span>Computer picks ${computerSelection}. Player picks ${playerSelection}</span>
+        <span ${result.includes('Player') ? 'class="green"' : ''}
+        ${result.includes('Computer') ? 'class="red"' : ''}>${result}</span>
+    `;
 
-// Step 5: Embrace mistakes as opportunities for learning and improvement, and fuel your curiosity by exploring new the wonders of frontend development. The vast tech world is filled with exciting discoveries waiting for you. Ask when you have the need or want to! Just keep coding!
+    htmlForComputer = `
+        <img class="move" src="./assets/${computerSelection}.png" alt="${computerSelection}">
+    `;
+
+    resultHTML.innerHTML = htmlForResult;
+    circleComputer.innerHTML = htmlForComputer;
+
+    updateScores();
+}
+
+
+function updateScores() {
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+}
+
+function handlePlayRound() {
+    playerMove = playerMove;
+    computerMove = computerPlay();
+    playRound(playerMove, computerMove);
+    localStorage.setItem(PREFIX_KEY +  'scores', JSON.stringify({ user: playerScore, computer: computerScore }));
+}
+
+function autoPlay() {
+    pickMove(computerPlay());
+    handlePlayRound();
+}
+
